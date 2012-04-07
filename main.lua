8635239de86dc830cd72c6f4f9cfda789571dcb9
Nathan Michalov <nmichalov@gmail.com>
2012-04-07 13:02:11 -0400

    shooped images and modified movement

--- main.lua
+++ main.lua
@@ -31,14 +31,15 @@ end
 function love.draw()
     mapTable = drawMap()
     love.graphics.draw(player.img, player.act_x, player.act_y, 1, 1)
+    keypress()
 end
 
 
-function love.keypressed()
+function keypress()
     if love.keyboard.isDown("down") then
         player.img = player.imgD
         if testMap(0, 2) then
-            player.grid_y = player.grid_y + 32
+            player.grid_y = player.grid_y + 32 
         end
     elseif love.keyboard.isDown("up") then
         player.img = player.imgU
@@ -55,7 +56,8 @@ function love.keypressed()
         if testMap(1, 1) then
             player.grid_x = player.grid_x + 32
         end
-    elseif love.keyboard.isDown(" ") then
+    end
+    if love.keyboard.isDown(" ") then
         local fx, fy = player.act_x, player.act_y
         if player.img == player.imgU then
             fy = fy - 64
@@ -68,13 +70,12 @@ function love.keypressed()
         end
         love.graphics.draw(fireball, fx, fy, 1, 1)
     end
-    print(player.grid_x, player.grid_y, player.grid_x/32, player.grid_y/32)
+    print(player.grid_x, player.grid_y, player.grid_x / 32, player.grid_y / 32)
 end
 
 
 function testMap(x,y)
-    spot = mapTable[(player.grid_x / 32) + x][(player.grid_y / 32) + y]
-    print(spot)
+    spot = mapTable[(player.grid_x / 32)+ x][(player.grid_y / 32) + y]
     if spot == '#' or spot == '^' then
         return false
     end
